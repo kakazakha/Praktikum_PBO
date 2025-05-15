100385f4fbfe40746520f3a905a602fc9a30fcc7
Kabis <kakazakha@gmail.com>
2025-05-15 17:26:20 +0700

    Add files via upload

---
+++ Pertemuan9/PostTestNew/src/Main.java
@@ -0,0 +1,68 @@
+class Vehicle {
+    public void startEngine() {
+        System.out.println("Mesin Nyala");
+    }
+}
+
+class Car extends Vehicle {
+    public void startEngine() {
+        System.out.println("Mesin Mobil Nyala");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    public void startEngine() {
+        System.out.println("Mesin Motor Nyala");
+    }
+}
+
+class License {
+    String nomorLisensi;
+    String tanggalBerlaku;
+
+    public License(String nomorLisensi, String tanggalBerlaku) {
+        this.nomorLisensi = nomorLisensi;
+        this.tanggalBerlaku = tanggalBerlaku;
+    }
+}
+
+class Driver {
+    String name;
+    Vehicle vehicle;
+    License license;
+
+    public Driver(String name, Vehicle vehicle, String nomorLisensi, String tanggalBerlaku) {
+        this.name = name;
+        this.vehicle = vehicle;
+        this.license = new License(nomorLisensi, tanggalBerlaku);
+    }
+
+    public void showInfo() {
+        System.out.println("Nama Pengemudi: " + name);
+        if (vehicle instanceof Car) {
+            System.out.println("Jenis Kendaraan: Mobil");
+        } else if (vehicle instanceof Motorcycle) {
+            System.out.println("Jenis Kendaraan: Motor");
+        } else {
+            System.out.println("Jenis Kendaraan: Tidak diketahui");
+        }
+        System.out.print("Status Mesin: ");
+        vehicle.startEngine();
+        System.out.println("Nomor Lisensi: " + license.nomorLisensi);
+        System.out.println("Tanggal Berlaku Lisensi: " + license.tanggalBerlaku);
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle car = new Car();
+        Vehicle motorcycle = new Motorcycle();
+
+        Driver driver1 = new Driver("Kaka", car, "DD3095", "2027-05-01");
+        Driver driver2 = new Driver("Kiki", motorcycle, "DD0304", "2028-01-15");
+
+        driver1.showInfo();
+        driver2.showInfo();
+    }
+}
