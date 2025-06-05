9235697287479b6d167268486c5e75c78aa8b407
Kabis <kakazakha@gmail.com>
2025-06-05 17:00:26 +0700

    Add files via upload

---
+++ Pertemuan11/AktivitasPraktikum/src/Pabrik.java
@@ -0,0 +1,3 @@
+public interface Pabrik {
+    void produksiKendaraan();
+}
---
+++ Pertemuan11/AktivitasPraktikum/src/Smartphone.java
@@ -0,0 +1,8 @@
+public class Smartphone implements Gadget{
+    
+    @Override
+    public void hidupkan() {
+        System.out.println("HP menyala");
+    }
+    
+}
